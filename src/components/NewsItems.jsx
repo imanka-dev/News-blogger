84e3797146f6e437f5e50d72b33fc58172015c5c
Imanka <imankasilvatharusha@gmail.com>
2025-12-21 14:21:52 +0530

    ✏ edit the newscard

--- src/components/NewsItems.jsx
+++ src/components/NewsItems.jsx
@@ -2,12 +2,12 @@
  import image from '../assets/Untitled-1.jpg'
  const NewsItems = ({ title, description, src, url }) => {
   return (
-   <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{maxWidth: "345px" }}>
-  <img src={src?src:image} style={{height:"200px",width:"327px"}} className="card-img-top" alt="..." />
-  <div className="card-body">
-    <h5 className="card-title">{title.slice(0,50)}</h5>
-    <p className="card-text">{description?description.slice(0,90):"No description available"}</p>
-    <a href={url} className="btn btn-primary">Read More</a>
+   <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{width: "345px", height: "450px" }}>
+  <img src={src?src:image} style={{height:"200px",width:"100%", objectFit:"cover"}} className="card-img-top" alt="..." />
+  <div className="card-body d-flex flex-column">
+    <h5 className="card-title" style={{minHeight: "3rem"}}>{title.slice(0,50)}</h5>
+    <p className="card-text flex-grow-1" style={{minHeight: "4.5rem"}}>{description?description.slice(0,90):"No description available"}</p>
+    <a href={url} className="btn btn-primary mt-auto">Read More</a>
   </div>
 </div>
   )
