5e03b90270bb4f38b3f23257d8f8b34a0a60296e
Imanka <imankasilvatharusha@gmail.com>
2025-12-21 16:56:01 +0530

    ✏vercel issue

--- src/components/NewsBoard.jsx
+++ src/components/NewsBoard.jsx
@@ -8,7 +8,16 @@ import NewsItems from "./NewsItems";
     useEffect(()=>{
         let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${import.meta.env.VITE_API_KEY}`;
 
-        fetch(url).then(response=> response.json()).then(data=> setArticles(data.articles));
+        fetch(url)
+            .then(response=> response.json())
+            .then(data=> {
+                if(data.articles) {
+                    setArticles(data.articles);
+                } else {
+                    console.error("API Error:", data);
+                }
+            })
+            .catch(error => console.error("Fetch error:", error));
 
     },[category] ) 
 
